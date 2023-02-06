9f5e385237c63c5c24ef2392ee152d20445011db
vincent4810 <vincent.michel1989@gmail.com>
2023-02-06 17:01:46 +0100

    récupération et traitement d'une URL

---
+++ Communication/UserData.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace Communication{
+
+    public class UserData{
+
+        public static void Main(string[] args){
+
+            string stringUrl = "";
+
+            int nbArguments;
+
+            if(args == null){
+                nbArguments = 0;
+            }else{
+                nbArguments = args.Length;
+            }
+
+            switch(nbArguments){
+                case 0:
+                    stringUrl = RequestUrl();
+                    break;
+                case 1:
+                    stringUrl = args[0];
+                    break;
+                default:
+                    Console.WriteLine("Veuillez exécuter le programme en spécifiant une url ou aucun argument");
+                    Environment.Exit(-1);
+                    break;
+            }
+
+            Console.WriteLine("Vérification de l'Url : " + stringUrl);
+
+            if(URLValide(stringUrl)){
+
+                Console.WriteLine(stringUrl + " est une URL Valide");
+            }else{
+
+                Console.WriteLine(stringUrl + " n'est pas une URL valide");
+            }
+        }
+
+        public static string RequestUrl(){
+            
+            Console.WriteLine("Veuillez saisir une URL valide");
+
+            string url = Console.ReadLine();
+
+            return url;
+        }
+
+        private static bool URLValide(string stringUrl){
+
+            if(Uri.IsWellFormedUriString(stringUrl, UriKind.Absolute)){
+
+                return true;
+            }else{
+
+                return false;
+            }
+        }
+    }
+}
